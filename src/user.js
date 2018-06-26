0c89047782a8dcded3bb589e54bda892f9722c99
Llewellyn Lovell <ll@locoblox.co>
2018-06-25 21:58:54 -0500

    added documentation

--- src/user.js
+++ src/user.js
@@ -3,15 +3,36 @@ import {
   CognitoUserAttribute,
 } from 'amazon-cognito-identity-js';
 
+/**
+ * Module used to authenticate a user
+ * and authorize privileges
+ * 
+ * @module User
+ */
 const User = function User() {
   const userPoolDetails = {
     UserPoolId: 'us-east-1_F3yVZA5Sp',
     ClientId: '7be3lv5eddcfpdv25kih5jd5mp',
   };
 
+  /**
+   * Function used to log a user in
+   *
+   * @function login
+   * @param {string} email - Users email address
+   * @param {string} password - Users password
+   */
   function login() {
   }
 
+  /**
+   * Function used to register a new user
+   *
+   * @function register
+   * @param {string} email - Users email address
+   * @param {string} password - Users password
+   * @param {string} phone - Users phone number
+   */
   function register(email, password, phone) {
     const userPool = new CognitoUserPool(userPoolDetails);
 
@@ -45,6 +66,12 @@ const User = function User() {
     });
   }
 
+  /**
+   * Function used to confirm a new user registration
+   *
+   * @function confirm
+   * @param {string} code - Users verification code
+   */
   function confirm() {
   }
 
